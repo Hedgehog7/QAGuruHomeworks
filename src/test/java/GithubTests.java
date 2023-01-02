7f78a82df8c389f2e0c9219671cddf3d25d3be89
Lidia Antonova <lidia.antonova@corp.mail.ru>
2023-01-02 22:39:15 +0300

    homework 2 done

---
+++ src/test/java/GithubTests.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubTests {
+    @Test
+    void FindJunit5Examples() {
+
+        //Open page with selenide in Github
+        open("https://github.com/selenide/selenide");
+        //Go to Wiki of the proj.
+        $$(".js-sidenav-container-pjax").findBy(text("Wiki")).click();
+        //Check that in the list of pages SoftAssertions page is present
+        $(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        //Open SoftAssertions
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        //Check that inside we have JUnit5 code examples
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+
+}
